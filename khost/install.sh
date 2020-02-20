93cd034ff4ac9bae084a1beb5aa0d12bce4e2abd
isx47595161 <adria.quintero@gmail.com>
2020-02-20 15:23:43 +0100

    docker hostp funciona

--- khost/install.sh
+++ khost/install.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
-cp /opt/docker/krb5.conf /etc/krb5.conf
-useradd local01
-useradd local02
-useradd user01
-useradd user02
-useradd user03
-useradd user04
-useradd user05
-useradd user06
+groupadd local01
+groupadd kusers
+useradd -g users -G local01 local01
+useradd -g users -G local01 local02
+useradd -g users -G local01 local03
+useradd -g users -G kusers user01
+useradd -g users -G kusers user02
+useradd -g users -G kusers user03
 echo "local01" | passwd --stdin local01
 echo "local02" | passwd --stdin local02
+echo "local03" | passwd --stdin local03
 
+cp /opt/docker/krb5.conf /etc/krb5.conf
+cp /opt/docker/system-auth /etc/pam.d/system-auth
--- khost/startup.sh
+++ khost/startup.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 /opt/docker/install.sh
+/bin/bash
